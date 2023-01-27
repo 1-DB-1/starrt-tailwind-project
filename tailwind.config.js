441255d2e77c125a12430faad1ce2b35fa0be847
Dragana Biljanovska <biljanovskadragana@gmail.com>
2023-01-27 13:09:04 +0100

    remove aos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,56 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    // extend: {
+    //   colors: {
+    //     'primary-red': '#C00014',
+    //     'fade-red': '#CA4854',
+    //     'dark-blue': '#161543',
+    //     'light-blue': '#0b3f86',
+    //   },
+    // },
+    animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        
+        blob: {
+          '0%, 100%': { transform: 'rotate(-25deg)' },
+          '50%': { transform: 'rotate(25deg)' },
+        }
+      },
+  },
+  plugins: [],
+  theme: {
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
+  
 }
+
+
+
+
